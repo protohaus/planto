66e64f07d9278598f4e5035df025444e69fc99fe
AntjeNett <62296763+AntjeNett@users.noreply.github.com>
2020-11-25 16:25:30 +0100

    Initial commit

---
+++ code/src/fan.cpp
@@ -0,0 +1,27 @@
+#include "fan.h"
+
+namespace planto {
+Fan::Fan(int pinPWM, int pinTacho, int channel)
+    : dutyCycleFan_(0),
+      pinPWM_(pinPWM),
+      pinTacho_(pinTacho),
+      fanFreq_(25000),
+      fanResolution_(8),
+      fanChannel_(1) {}
+
+void Fan::updateSpeed() { 
+      ledcWrite(fanChannel_, dutyCycleFan_); 
+      Serial.println("in updateSpeed");
+      Serial.println(dutyCycleFan_);
+      }
+
+void Fan::init() {
+  pinMode(pinPWM_, OUTPUT);
+  pinMode(pinTacho_, INPUT);
+
+  ledcSetup(fanChannel_, fanFreq_, fanResolution_);
+
+  ledcAttachPin(pinPWM_, fanChannel_);
+}
+
+}  // namespace planto
